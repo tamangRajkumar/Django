a5dbb5be510e6a66c01febd18ef7c73232859fac
DELL <rajkumartamang182@gmail.com>
2022-07-06 13:22:41 +0545

    updated

--- myDjangoProject/auth_app/views.py
+++ myDjangoProject/auth_app/views.py
@@ -1,3 +1,7 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def register(response):
+        return response.HTTPResponse("Register User")
--- myDjangoProject/myDjangoProject/urls.py
+++ myDjangoProject/myDjangoProject/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from urllib import response
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include("auth_app.urls"))
 
 ]
